ef7862bb75400a1005c11ba1ab255c36f64dba46
Phil Oye <philoye@philoye.com>
2010-11-14 03:05:21 +1100

    Moved the mongdo config up a level. Both server and consumer will use it.

--- consumer/balloon_receiver.rb
+++ consumer/balloon_receiver.rb
@@ -3,7 +3,7 @@ require 'mongoid'
 require './kubrick/balloon_receiver'
 require './kubrick/balloon_fix'
 
-file_name = File.join(File.dirname(__FILE__), "config", "mongoid.yml")
+file_name = File.join(File.dirname(__FILE__), "..", "config", "mongoid.yml")
 @settings = YAML.load(ERB.new(File.new(file_name).read).result)
 
 Mongoid.configure do |config|
