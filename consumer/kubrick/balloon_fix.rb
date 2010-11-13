5337686ccd10b9397279b0165946e809b3e9c307
Phil Oye <philoye@philoye.com>
2010-11-13 20:30:17 +1100

    works!

---
+++ consumer/kubrick/balloon_fix.rb
@@ -0,0 +1,7 @@
+class BalloonFix
+  # include Mongoid::Document
+  
+  def self.create_from_csv(buffer)
+    puts buffer
+  end
+end
---
+++ consumer/kubrick/balloon_receiver.rb
@@ -0,0 +1,43 @@
+require 'serialport'
+
+class BalloonReceiver
+  
+  attr_reader :port, :baudrate, :bitlength, :stopbit
+  
+  def initialize
+    @port = 0
+    @baudrate = 115200
+    @bitlength = 8
+    @stopbit = 1
+    
+    clean_logs
+  end
+  
+  def clean_logs
+    `rm log/*`
+  end
+
+  def serial_port
+    @serial_port ||= SerialPort.new(0, 115200, 8, 1, SerialPort::NONE)
+  end
+
+  def go
+    begin
+      while true do
+        log.write(c = serial_port.getc)
+        if c == "\n"
+          log.flush 
+        end
+      end
+
+    ensure
+      log.close
+      serial_port.close
+    end
+  end
+  
+  def log
+    @f ||= File.new("./log/launch-#{Time.now.strftime("%Y%m%d%H%M%S")}.log", "w")    
+  end
+end
+  
