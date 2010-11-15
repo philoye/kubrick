370b171a534c564ae9a6f2366f98c1b82b986d7d
Phil Oye <philoye@philoye.com>
2010-11-15 14:51:46 +1100

    Some more front end tweaks. Placeholder altitude graph

--- server/public/kubrick.js
+++ server/public/kubrick.js
@@ -1,8 +1,33 @@
-(function() {
-  window.log = function() {
-    log.history = log.history || [];
-    log.history.push(arguments);
-    return (this.console) ? console.log(Array.prototype.slice.call(arguments)) : null;
-  };
-  $(function() {});
-}).call(this);
+window.onload = function () {
+  var r = Raphael("altitude-graph");
+  var x = [], y = [];
+  for (var i = 0; i < 1e6; i++) {
+      x[i] = i * 10;
+      y[i] = (y[i - 1] || 0) + (Math.random() * 7) - 3;
+  }
+  r.g.linechart(-10, 0, 970, 180, x, y, {shade: true});
+};
+
+
+(function($){
+
+  $(document).ready(function(){
+
+
+  });
+
+
+
+})(this.jQuery);
+
+
+
+// usage: log('inside coolFunc',this,arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
+};
--- server/views/kubrick.coffee
+++
@@ -1,9 +0,0 @@
-window.log = () ->
-  log.history = log.history || [] # store logs to an array for reference
-  log.history.push(arguments)
-  if(this.console)
-    console.log( Array.prototype.slice.call(arguments) );
-
-
-$ ->
-  
